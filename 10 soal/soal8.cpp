6965a50e7eafca165096761303ab5fc760529af4
XenoidXD <fadhlanguanteng@gmail.com>
2025-12-03 23:07:23 +0700

    add totalTim logic

--- 10 soal/soal8.cpp
+++ 10 soal/soal8.cpp
@@ -2,19 +2,32 @@
 #include <string>
 using namespace std;
 
-void inputTim(string tim[], int &jumlah) {
+int iKosong=0;
+int totalTim = 0;
+
+void inputTim(string tim[]) {
+    int jumlahTim;
     cout << "Masukkan jumlah tim: ";
-    cin >> jumlah;
+    cin >> jumlahTim;
     cin.ignore();
-    for (int i = 0; i < jumlah; i++) {
+
+    for (int i = 0; i < jumlahTim; i++) {
         cout << "Nama tim ke-" << i + 1 << " : ";
-        getline(cin, tim[i]);
+        getline(cin, tim[iKosong]);
+        iKosong++;
+        totalTim++;
     }
 }
 
-void tampilTim(string tim[], int jumlah) {
+void tampilTim(string tim[]) {
+    if (totalTim == 0) {
+        cout << "Belum ada data tim yang diinput." << endl;
+        return;
+    }
+
     int i = 0;
-    while (i < jumlah) {
+    cout << "Daftar tim peserta Piala Dunia:" << endl;
+    while (i < totalTim) {
         cout << "Tim ke-" << i + 1 << " : " << tim[i] << endl;
         i++;
     }
@@ -22,12 +35,12 @@ void tampilTim(string tim[], int jumlah) {
 
 int main() {
     string tim[50];
-    int jumlah = 0;
     int pilih;
     bool jalan = true;
 
     while (jalan) {
-        cout << "Menu Pengelolaan Tim Piala Dunia" << endl;
+        cout << endl;
+        cout << "=== PENGELOLAAN DATA TIM PIALA DUNIA ===" << endl;
         cout << "1. Input data tim" << endl;
         cout << "2. Tampilkan data tim" << endl;
         cout << "3. Keluar" << endl;
@@ -35,17 +48,19 @@ int main() {
         cin >> pilih;
         cin.ignore();
 
-        if (pilih == 1) {
-            inputTim(tim, jumlah);
-        } else if (pilih == 2) {
-            tampilTim(tim, jumlah);
-        } else if (pilih == 3) {
-            jalan = false;
-        } else {
-            cout << "Menu tidak valid" << endl;
+        switch (pilih) {
+            case 1:
+                inputTim(tim);
+                break;
+            case 2:
+                tampilTim(tim);
+                break;
+            case 3:
+                jalan = false;
+                break;
+            default:
+                cout << "Menu tidak valid." << endl;
         }
-
-        cout << endl;
     }
 
     return 0;
